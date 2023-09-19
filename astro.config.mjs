234b885a9243a6ddf5b22219e6c1966dabb83bcf
stublic.jurica <stublic.jurica@gmail.com>
2023-09-19 09:19:35 +0200

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(), react(), preact({ compat: true })]
 });
--- src/utils/all.js
+++ src/utils/all.js
@@ -7,3 +7,11 @@ export const getFormattedDate = (date) =>
         day: "numeric",
       })
     : "";
+
+    export default function StaticTimePickerLandscape() {
+      return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <StaticTimePicker orientation="landscape" />
+        </LocalizationProvider>
+      );
+    }
